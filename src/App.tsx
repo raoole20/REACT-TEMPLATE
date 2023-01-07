4118be888fb13779053916ff2ee3acd149bb7878
raoole <espinaraul@gmail.com>
2023-01-07 01:10:17 -0400

    saved

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { Box } from "@mui/material"
 
-// My Imports ----------------------
+// My Imports -------------------------
 import "./assets/sass/tailwind.scss"
+import Todo from "./testcomponents/Todo"
 
 function App() {
   return (
     <Box 
       className="box-layout"
       height={'100vh'} 
-      width={'100%'}>
-      
+      width={'100%'}
+      >
+      <Todo />
     </Box>
   )
 }
